5152eb3037982c1daeb51562267cb4baae40b7c0
Amr Emad <aemaddin@gmail.com>
2019-05-09 16:45:14 +0200

    fixed route namespace for controller

--- src/ZohoServiceProvider.php
+++ src/ZohoServiceProvider.php
@@ -68,7 +68,7 @@ class ZohoServiceProvider extends ServiceProvider
     {
         return [
             'prefix' => '/',
-            'namespace' => 'aemaddin\Zoho\Controllers',
+            'namespace' => 'Aemaddin\Zoho\Controllers',
         ];
     }
 
